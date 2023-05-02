5d422026a16373e351445bcc7ff0422971a34361
Sami Hindi <hindis@bzz.ch>
2023-05-02 11:46:08 +0000

    Added Express First Exercice

---
+++ Exercices/Express/hello-world.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello World!');
+})
+
+app.listen(port, () => {
+    console.log(`Server gestartet auf => http://localhost:${port}`);
+});
